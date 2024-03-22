b093e857dfb0c4f8be09d8e956022d6bd18f54e1
Nadir <nadirbtssio@gmail.com>
2024-03-22 15:27:48 +0100

    add deploy on Azure 30

--- back/src/routes/health.js
+++ back/src/routes/health.js
@@ -1,7 +1,18 @@
 const healthRoute = ({ app }) => {
   app.get("/", async (req, res) => {
-    return res.status(200).send("OK")
-  })
-}
+    return res.status(200).send("OK");
+  });
 
-export default healthRoute
+  app.get("/log", (req, res) => {
+    fs.readFile("logfile.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Erreur lors de la lecture du fichier de log");
+        return;
+      }
+      res.send(data);
+    });
+  });
+};
+
+export default healthRoute;
